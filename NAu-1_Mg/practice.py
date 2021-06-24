fe000d699a7b75e84a2e0bc21f8ec0fbd33c921e
sarahvs99 <86365575+sarahvs99@users.noreply.github.com>
2021-06-24 16:10:55 +0100

    Update practice.py

--- NAu-1_Mg/practice.py
+++ NAu-1_Mg/practice.py
@@ -8,6 +8,7 @@ Created on Thu Jun 24 11:11:04 2021
 
 import MDAnalysis as mda
 from MDAnalysis.analysis import lineardensity as lin
+from MDAnalysis.analysis import density
 from MDAnalysis.topology import tpr
 from MDAnalysis.topology import TPRParser
 
@@ -32,13 +33,43 @@ print("Maximum z-coordinate of clay is ", clay_pos_max [2])
 print("Minimum z-coordinate of clay is ", clay_pos_min [2]) 
 
 # assign bulk ions AtomGroup
-dynamic_ions=mda.AtomGroup(N1_Mg_eqd.select_atoms('(resname Mg or resname Cl) and (prop z > 89.049995 or prop z < 43.16)', updating=True))
+dynamic_ions=mda.AtomGroup(N1_Mg_eqd.select_atoms('(resname Mg or resname Cl) and (prop z > 89.36 or prop z < 43.18)', updating=True))
+dynamic_cations=mda.AtomGroup(N1_Mg_eqd.select_atoms('(resname Mg) and (prop z > 89.36 or prop z < 43.18)', updating=True))
+dynamic_anions=mda.AtomGroup(N1_Mg_eqd.select_atoms('(resname Cl) and (prop z > 89.36 or prop z < 43.18)', updating=True))
 
 # ionic z-density profiles
 ionic_density=lin.LinearDensity(dynamic_ions, binsize=0.25).run()
+cationic_density=lin.LinearDensity(dynamic_cations, binsize=0.25).run()
+anionic_density=lin.LinearDensity(dynamic_anions, binsize=0.25).run()
 
-plt.plot(np.linspace(0, 133.3808, 534), ionic_density.results['z']['char'])
-plt.xlabel("z-coordinate (Å)")
-plt.ylabel("Charge density")
-plt.title("ionic z-density profile")
+#%%
+fig, ion_dens=plt.plot(np.linspace(0, 133.3808, 534), ionic_density.results['z']['char'])
+ions_dens.set_xlabel("z-coordinate (Å)")
+ions_dens.set_ylabel("Charge density")
+ion_dens.set_title("ionic z-density profile")
 
+#%%
+fig, cat_dens=plt.plot(np.linspace(0, 133.3808, 534), cationic_density.results['z']['char'])
+cat_dens.set_xlabel("z-coordinate (Å)")
+cat_dens.set_ylabel("Charge density")
+cat_dens.set_title("cationic z-density profile")
+
+#%%
+fig, ani_dens=plt.plot(np.linspace(0, 133.3808, 534), anionic_density.results['z']['char'])
+ani_dens.set_xlabel("z-coordinate (Å)")
+ani_dens.set_ylabel("Charge density")
+ani_dens.set_title("anionic z-density profile")
+
+#%%
+fig, cat_pos=plt.plot(np.linspace(0, 133.3808, 534), cationic_density.results['z']['pos'])
+cat_pos.set_xlabel("z-coordinate (Å)")
+cat_pos.set_ylabel("Position density")
+cat_pos.set_title("cationic z-density profile")
+
+#%%
+fig, cat=plt.subplots()
+cat.plot(np.linspace(0, 133.3808, 534), cationic_density.results['z']['char'], '--', color='black', label="charge density")
+cat.plot(np.linspace(0, 133.3808, 534), cationic_density.results['z']['pos'], ':', label="mass density",)
+cat.set_xlabel("z-coordinate (Å)")
+cat.set_ylabel("density")
+cat.legend()
