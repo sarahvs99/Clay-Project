75081010bf205095e57981418fe024b8857e772a
sarahvs99 <sarahvs448@gmail.com>
2021-06-25 16:59:13 +0100

    new trial file

---
+++ NAu-1_Mg/trial2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 25 15:39:04 2021
+
+@author: sarah
+"""
+
+# Average radial distribution function for two groups of atoms
+
+
+
